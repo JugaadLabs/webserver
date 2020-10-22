306b653971432e2d06bec9ee62166c4d03740b79
arnav dhamija <arnav.dhamija@gmail.com>
2020-10-22 17:49:52 -0400

    demo IPC code

---
+++ src/DemoProcess.py
@@ -0,0 +1,19 @@
+import numpy as np
+from src.zmq_utils import zmqNode
+import time
+from tqdm import tqdm
+
+def main():
+    inputResolution = (480, 640)
+
+    sendImgNode = zmqNode('send', 9500)
+    recvResultsNode = zmqNode('recv', 9501)
+
+    for i in tqdm(range(10000)):
+        img = np.random.rand((480,640,3))
+        sendImgNode.send_array(img)
+        dataDict = recvResultsNode.recv_zipped_pickle()
+        print(dataDict['birdsView'].shape)
+
+if __name__ == "__main__":
+    main()
