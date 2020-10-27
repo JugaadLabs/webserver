bd0235f6f20a1082bfdcb0bc59935b3b13461e99
arnav dhamija <arnav.dhamija@gmail.com>
2020-10-27 16:40:34 -0400

    improved error handling

--- src/DetectionProcess.py
+++ src/DetectionProcess.py
@@ -18,24 +18,28 @@ def main():
     monoDist = monoDistance(
         inputResolution, birdsEyeResolution, enginePath, detection_class_name_3cls, np.array(range(3)))
 
-    receiveImgNode = zmqNode('recv', 9500)
-    sendResultsNode = zmqNode('send', 9501)
-
-    while True:
-        try:
-            img = receiveImgNode.recv_array()
-        except zmq.error.Again as e:
-            pass
-        else:
-            img, birds_view_img, selected_bboxs, bbox_distances = monoDist.detection_birdsview(
-                img, vis_thresh, nms_iou_thresh, box_area_thresh)
-            dataDict = {
-                "img": img,
-                "birdsView": birds_view_img,
-                "selectedBboxes": selected_bboxs,
-                "bboxDistances": bbox_distances
-            }
-            sendResultsNode.send_zipped_pickle(dataDict)
+    try:
+        receiveImgNode = zmqNode('recv', 9500)
+        sendResultsNode = zmqNode('send', 9501)
+    except zmq.error.ZMQError:
+        print("Detection Process already running")
+        return
+    else:
+        while True:
+            try:
+                img = receiveImgNode.recv_array()
+            except zmq.error.Again as e:
+                pass
+            else:
+                img, birds_view_img, selected_bboxs, bbox_distances = monoDist.detection_birdsview(
+                    img, vis_thresh, nms_iou_thresh, box_area_thresh)
+                dataDict = {
+                    "img": img,
+                    "birdsView": birds_view_img,
+                    "selectedBboxes": selected_bboxs,
+                    "bboxDistances": bbox_distances
+                }
+                sendResultsNode.send_zipped_pickle(dataDict)
 
 
 if __name__ == "__main__":
