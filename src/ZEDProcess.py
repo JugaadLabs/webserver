53b9c297fc08528a131d8c295378777b0828bedd
arnav dhamija <arnav.dhamija@gmail.com>
2020-11-12 11:43:20 -0500

    added new zedprocess code

---
+++ src/ZEDProcess.py
@@ -0,0 +1,78 @@
+import numpy as np
+import cv2
+import datetime
+import signal
+import time
+import sys
+import pyzed.sl as sl
+import os
+import threading
+
+class ZEDProcess:
+    def intializeCamera(self, initParams):
+        self.cam = sl.Camera()
+        status = self.cam.open(initParams)
+        if status != sl.ERROR_CODE.SUCCESS:
+            print(repr(status))
+            self.cam = None
+
+    def startRecording(self):
+        if self.recordEvent.is_set() == False:
+            self.startTime = datetime.datetime.now()
+            self.startUnixTime = time.time()
+
+            startTimeString = self.startTime.strftime("ZED_%Y-%m-%d-%H-%M-%S")
+            self.filename = startTimeString+".svo"
+            filepath = os.path.join(self.dir, self.filename)
+            print("ZED Camera - recording to " + filepath)
+
+            recording_param = sl.RecordingParameters(filepath, sl.SVO_COMPRESSION_MODE.H264)
+            self.cam.enable_recording(recording_param)
+        self.recordEvent.set()
+
+    def stopRecording(self):
+        if self.recordEvent.is_set():
+            self.cam.disable_recording()
+        self.recordEvent.clear()
+
+    def recordFrame(self):
+        if (time.time() - self.startUnixTime > self.recordingInterval):
+            self.stopRecording()
+            self.startRecording()
+
+    def commandLoop(self, cam, commandQueue, recordEvent):
+        while True:
+            while not commandQueue.empty():
+                command = commandQueue.get()
+                if command == 'REC':
+                    self.startRecording()
+                elif command == 'STOP':
+                    self.stopRecording()
+
+    def run(self, initParams, dir, recordingInterval, commandQueue, imageQueue, recordEvent, terminateEvent):
+        self.intializeCamera(initParams)
+        self.dir = dir
+        self.terminateEvent = terminateEvent
+        self.recordEvent = recordEvent
+        self.recordingInterval = recordingInterval
+
+        if self.cam == None:
+            print("ZED Error, terminating process.")
+            return
+        while terminateEvent.is_set() == False:
+            runtime = sl.RuntimeParameters()
+            self.cam.grab(runtime)
+            image = sl.Mat()
+            self.cam.retrieve_image(image, sl.VIEW.LEFT)
+
+            lastFrame = image.get_data()
+            lastFrame = cv2.rotate(lastFrame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            # replace with Q
+            imageQueue.put(lastFrame)
+
+            # PAUSE is currently ignored, since disabling cam.grab would disable the stream
+            if recordEvent.is_set() == True:
+                self.recordFrame()
+        if recordEvent.is_set() == True:
+            self.cam.disable_recording()
+        self.cam.close()
--- src/ZEDStreamer.py
+++ src/ZEDStreamer.py
@@ -7,75 +7,43 @@ import sys
 import pyzed.sl as sl
 import os
 import cherrypy
+import multiprocessing
+
+from src.ZEDProcess import ZEDProcess
 
 
 class ZEDStreamer:
     def __init__(self, frameLock, dir, recordingInterval, resolution, depth, framerate):
-        self.initParams = sl.InitParameters()
-        self.initParams.camera_resolution = resolution
-        self.initParams.depth_mode = depth
-        self.initParams.camera_fps = framerate
-        self.lastFrame = None
-        self.dir = dir
-        self.frameLock = frameLock
-        self.RECORDING = False
-        self.recordingInterval = recordingInterval
-        self.cam = self.intializeCamera()
-        self.filename = ""
+        initParams = sl.InitParameters()
+        initParams.camera_resolution = resolution
+        initParams.depth_mode = depth
+        initParams.camera_fps = framerate
 
-    def intializeCamera(self):
-        cam = sl.Camera()
-        status = cam.open(self.initParams)
-        if status != sl.ERROR_CODE.SUCCESS:
-            print(repr(status))
-            return None
-        else:
-            return cam
+        self.lastFrame = None
+
+        self.recordEvent = multiprocessing.Event()
+        self.terminateEvent = multiprocessing.Event()
+        self.commandQueue = multiprocessing.Queue()
+        self.imageQueue = multiprocessing.Queue()
+        multiprocessing.Process(target=ZEDProcess.run, args=(
+            initParams, dir, recordingInterval, self.commandQueue, self.imageQueue, self.recordEvent, self.terminateEvent))
 
     def startRecording(self, startTime):
-        if self.RECORDING == False:
-            self.startTime = startTime
-            self.startUnixTime = time.time()
+        self.commandQueue.put('REC')
+        self.startTime = startTime
+        self.startUnixTime = time.time()
 
-            startTimeString = self.startTime.strftime("ZED_%Y-%m-%d-%H-%M-%S")
-            self.filename = startTimeString+".svo"
-            filepath = os.path.join(self.dir, self.filename)
-            print("ZED Camera - recording to " + filepath)
-
-            recording_param = sl.RecordingParameters(filepath, sl.SVO_COMPRESSION_MODE.H264)
-            self.cam.enable_recording(recording_param)
-        self.RECORDING = True
+        startTimeString = self.startTime.strftime("ZED_%Y-%m-%d-%H-%M-%S")
+        self.filename = startTimeString+".svo"
 
     def isRecording(self):
-        return self.RECORDING
+        return self.recordEvent.is_set()
 
     def stopRecording(self):
-        if self.RECORDING == True:
-            self.cam.disable_recording()
-        self.RECORDING = False
-
-    def recordFrame(self):
-        if (time.time() - self.startUnixTime > self.recordingInterval):
-            self.stopRecording()
-            now = datetime.datetime.now()
-            self.startRecording(now)
+        self.commandQueue.put('STOP')
 
     def run(self):
-        if self.cam == None:
-            return
         while True:
-            runtime = sl.RuntimeParameters()
-            self.cam.grab(runtime)
-            image = sl.Mat()
-            self.cam.retrieve_image(image, sl.VIEW.LEFT)
-            self.frameLock.acquire()
-            self.lastFrame = image.get_data()
-            self.lastFrame = cv2.rotate(self.lastFrame, cv2.ROTATE_90_COUNTERCLOCKWISE)
-            cherrypy.engine.publish("zedFrame", self.lastFrame)
-            self.frameLock.release()
-            # PAUSE is currently ignored, since disabling cam.grab would disable the stream
-            if self.RECORDING == True:
-                self.recordFrame()
-        if self.RECORDING == True:
-            self.cam.disable_recording()
-        self.cam.close()
+            while not self.imageQueue.empty():
+                self.lastFrame = self.imageQueue.get()
+                cherrypy.engine.publish("zedFrame", self.lastFrame)
